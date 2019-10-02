eb45851ddf45ced24af8be0c9a1b516a8c54f3d0
Jacqui Bush <jacqui@stainedglassofmckinney.com>
2019-10-02 14:34:41 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -10,9 +10,9 @@ end
  def position_taken?(board, index)
    if (board[index] == " ") || (board[index] == "") || (board[index] == nil)
       return false 
-   elsif 
-     boad[index] == "X" || board[index] == "O"
-     return true
+ #  elsif 
+#     boad[index] == "X" || board[index] == "O"
+ #    return true
    end
    
  end
