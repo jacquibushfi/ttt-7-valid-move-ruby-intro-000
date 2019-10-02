07854891a587a1425429c9e3830bdab0e682bea7
Jacqui Bush <jacqui@stainedglassofmckinney.com>
2019-10-02 14:28:30 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,7 +2,7 @@
 def valid_move?(board, index)
   if position_taken?(board, index) == false && index.between?(0,8)
     return true
-  elsif board[index] == "X" || board[index] == "O"
+  else
     return false
   end
 end
